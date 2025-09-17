2ee32a3bc2aabdf56bdce04b1cf837eaaa77edb8
guihuatu2022 <102854599+guihuatu2022@users.noreply.github.com>
2025-09-17 13:42:58 +0800

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
+darkMode: 'class',
+theme: {
+extend: {
+colors: {
+brand: {
+50: '#f3faf8',
+100: '#e6f7f0',
+500: '#0f766e'
+}
+}
+}
+},
+plugins: [],
+}
